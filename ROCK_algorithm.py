7492985009580e9e5751890fe7a6d03b92eb9dac
Turbobolekus <adam01wroblewski@gmail.com>
2024-05-05 22:38:44 +0200

    class implementation

--- ROCK_algorithm.py
+++ ROCK_algorithm.py
@@ -5,9 +5,10 @@ class ROCK:
         self.similarity_matrix = None
         self.adjacency_matrix = None
         self.links_matrix = None
+        self.clusters = None
     
 
-    def jaccard_coefficient(self, a, b):
+    def calculate_jaccard_coefficient(self, a, b):
         A = set(a)
         B = set(b)
         intersection = A.intersection(B)
@@ -19,48 +20,48 @@ class ROCK:
             return len(intersection)/len(sum)
 
 
-    def get_similiarity_matrix(self, data):
+    def calculate_similiarity_matrix(self, data):
         points_num = len(data)
         matrix = np.zeros((points_num,points_num))
         for i in range(0, points_num):
             a = data[i]
             for j in range(i+1,points_num):
                 b = data[j]
-                matrix[i][j] = self.jaccard_coefficient(a,b)
-                matrix[j][i] = self.jaccard_coefficient(a,b)
+                matrix[i][j] = self.calculate_jaccard_coefficient(a,b)
+                matrix[j][i] = self.calculate_jaccard_coefficient(a,b)
             matrix[i][i] = 1
-        return matrix
+        self.similarity_matrix = matrix
 
-
-    def get_adjacency_matrix(self, data, phi):
+    def calculate_adjacency_matrix(self, data, phi):
         points_num = len(data)
         matrix = np.zeros((points_num,points_num))
-        similiarity_matrix = self.get_similiarity_matrix(data)
+        self.calculate_similiarity_matrix(data)
         for i in range(0, points_num):
             for j in range(i+1,points_num):
-                if similiarity_matrix[i][j] >= phi:
+                if self.similarity_matrix[i][j] >= phi:
                     matrix[i][j] = 1
                     matrix[j][i] = 1
             matrix[i][i] = 1
-        return matrix
+        self.adjacency_matrix = matrix
 
 
     def get_links_matrix(self, adjacency_matrix):
         links_matrix = np.matmul(adjacency_matrix, adjacency_matrix)
         np.fill_diagonal(links_matrix,0)
-        return links_matrix
+        self.links_matrix = links_matrix
+        # return links_matrix
 
 
-    def get_link_for_clusters(self, cluster_a, cluster_b, link_matrix):
+    def get_link_for_clusters(self, cluster_a, cluster_b):
         links = 0
         for point_a_index in cluster_a:
             for point_b_index in cluster_b:
-                links += link_matrix[point_a_index][point_b_index]
+                links += self.links_matrix[point_a_index][point_b_index]
         return links
 
 
-    def clusters_goodness(self, cluster_a, cluster_b, link_matrix, theta):
-        link_num = self.get_link_for_clusters(cluster_a, cluster_b, link_matrix)
+    def clusters_goodness(self, cluster_a, cluster_b, theta):
+        link_num = self.get_link_for_clusters(cluster_a, cluster_b)
         n_a = len(cluster_a)
         n_b = len(cluster_b)
         f = (1-theta)/(1+theta)
@@ -71,15 +72,15 @@ class ROCK:
         return goodness
 
 
-    def get_clusters_to_merge(self, all_clusters, link_matrix, theta):
+    def get_clusters_to_merge(self, theta):
         max_goodness = 0
         a_cluster_to_merge = None
         b_cluster_to_merge = None 
-        for i in range(len(all_clusters)):
-            a = all_clusters[i]
-            for j in range(i+1, len(all_clusters)):
-                b = all_clusters[j]
-                goodness = self.clusters_goodness(a, b, link_matrix, theta)
+        for i in range(len(self.clusters)):
+            a = self.clusters[i]
+            for j in range(i+1, len(self.clusters)):
+                b = self.clusters[j]
+                goodness = self.clusters_goodness(a, b, theta)
                 if(goodness > max_goodness):
                     max_goodness = goodness
                     a_cluster_to_merge = i
@@ -89,21 +90,28 @@ class ROCK:
 
     def get_clusters(self, data, phi, cluster_num, theta = 0.5):
         theta = phi
-        adjacency_matrix = self.get_adjacency_matrix(data, phi)
-        links_matrix = self.get_links_matrix(adjacency_matrix)
-        clusters = []
+        self.calculate_adjacency_matrix(data, phi)
+        self.get_links_matrix(self.adjacency_matrix)
+        self.clusters = []
         for i in range(len(data)):
-            clusters.append([i])
+            self.clusters.append([i])
 
-        while (len(clusters) > cluster_num):
-            clusters_to_merge = self.get_clusters_to_merge(clusters, links_matrix, theta)
+        while (len(self.clusters) > cluster_num):
+            clusters_to_merge = self.get_clusters_to_merge(theta)
             if clusters_to_merge != (None, None):
                 a = clusters_to_merge[0]
                 b = clusters_to_merge[1]
-                clusters[a] += clusters[b]
-                clusters.pop(b)
+                self.clusters[a] += self.clusters[b]
+                self.clusters.pop(b)
             else:
                 break
-        return clusters
+        return self.clusters
     
-    # def get_clusters_items(self, clusters, )
+    def get_clusters_points(self, data, clusters):
+        clusters_points = []
+        for cluster in clusters:
+            cluster_list = []
+            for point_index in cluster:
+                cluster_list.append(tuple(data[point_index]))
+            clusters_points.append(cluster_list)
+        return clusters_points
--- test2.py
+++ test2.py
@@ -10,3 +10,7 @@ data = list(reader)
 my_rock = ROCK()
 clusters = my_rock.get_clusters(data, 0.2, 2)
 print(clusters)
+
+clusters_points = my_rock.get_clusters_points(data,clusters)
+print(clusters_points)
+print("dupa")
