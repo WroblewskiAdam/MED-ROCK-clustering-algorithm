e762e7bfccb5b4c13819343dd83f429cfee0ad55
Turbobolekus <92735592+WroblewskiAdam@users.noreply.github.com>
2024-05-29 20:57:44 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,49 +1,11 @@
+import csv
+from scipy.io import arff
+import pandas as pd
 from ROCK_algorithm import ROCK
-import csv
-from scipy.io import arff
-
-import pandas as pd
-
-zoo, meta = arff.loadarff('zoo.arff')
-df = pd.DataFrame(zoo)
-a = df.head()
-print(a);
-print(meta)
-print(a.dtypes)
-# print(df.shape[0])
-# print(df)
-# a.pop(a.columns[-1])
-# print(a.iloc[2])
-
-
-b = a.values.tolist()
-print(b)
-
-
-file = open('dataset3.csv')
-reader = csv.reader(file)
-data = list(reader)
-# print(data)
-
-
-# my_rock = ROCK()
-# clusters = my_rock.get_clusters(data, 0.2, 2)
-# print(clusters)
-
-# clusters_points = my_rock.get_clusters_points(data,clusters)
-
-
-
-
-
-
-import csv
-from scipy.io import arff
-
-import pandas as pd
 
 
 def convert(df, column_name, attribures_list):
+    # rozbicie kolumny wieloatrybutowej
     column = df[column_name]
     column_index = df.columns.tolist().index(column_name)
     df = df.drop(columns = [column_name])
@@ -52,46 +14,68 @@ def convert(df, column_name, attribures_list):
         attribute_value = attribures_list[i]
         for point in column:
             values.append(1) if point == attribute_value else values.append(0)
-        df.insert(column_index + i, column_name + str(attribute_value), values)    
+        df.insert(column_index + i, column_name + str(attribute_value), values)
+
+    # zmiana typu kolumny klasy
+    df_mod = df.select_dtypes([object]).stack().str.decode('utf-8').unstack()
+    df["class"] = df_mod["class"]
+    # konwersja danych do int
+    df = df.astype(int)
+
     return df
     
+def split_data_class(list_data):
+    # print("dupa")
+    # for i in list_data:
+    #     print(i)
+    for i in range(len(list_data)):
+        list_data[i] = [list_data[i][:-1], list_data[i][-1]]
+
+    return list_data
+    
+def convert_data_to_index(list_data):
+    for i in range(len(list_data)):
+        point_data = list_data[i][0]
+        point_data_index = []
+        for j in range(len(point_data)):
+            if int(point_data[j]) == 1: point_data_index.append(j)
+        list_data[i][0] = point_data_index
+    return list_data
+
 
 
 zoo, meta = arff.loadarff('zoo.arff')
 df = pd.DataFrame(zoo)
 
-df = convert(df, "LEGS", [0,1,2,3,4,5,6,7,8])
-print(df.head());
-df_mod = df.select_dtypes([object]).stack().str.decode('utf-8').unstack()
-df["class"] = df_mod["class"]
-df = df.astype(int)
-print(df.head());
-print(df.dtypes)
+df = pd.read_csv('dataset3.csv')
+
+with open('dataset3.csv', newline='') as f:
+    reader = csv.reader(f)
+    data = list(reader)
+for i in data :
+    print(i)
+
+# df = convert(df, "LEGS", [0,1,2,3,4,5,6,7,8])
+data = split_data_class(data)
+for i in data :
+    print(i)
+data = convert_data_to_index(data)
+
+for i in data :
+    print(i)
 
 
-list_data = df.values.tolist()
-for i in range(10):
-    print(list_data[i])
-    
-for i in range(len(list_data)):
-    # print(list_data[i])
-    # print(list_data[i][:-1])
-    # print(list_data[i][-1])
-    list_data[i] = [list_data[i][:-1], list_data[i][-1]]
-    # print(list_data[i])
+my_rock = ROCK()
 
-for i in range(len(list_data)):
-    point_data = list_data[i][0]
-    point_data_index = []
-    for j in range(len(point_data)):
-        if point_data[j] == 1: point_data_index.append(j)
-    list_data[i][0] = point_data_index
-    
-for i in range(10):
-    print(list_data[i])
-# df.to_csv('zoo.csv', encoding='utf-8', index=False)
+data_without_indexes = []
+for point in data:
+    data_without_indexes.append(point[0])
 
+clusters = my_rock.get_clusters(data_without_indexes, 0.2, 2)
+for cluster in clusters:
+    cluster.sort()
+print(clusters)
 
-
+clusters_points = my_rock.get_clusters_points(data,clusters)
 # print(clusters_points)
-# print("dupa")
+
