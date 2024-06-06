86f297ad3efc1fa550bb079153f7bd83e580af08
Turbobolekus <adam01wroblewski@gmail.com>
2024-06-06 10:28:31 +0200

    clear

--- ROCK.py
+++ ROCK.py
@@ -1,111 +1,175 @@
 import numpy as np
-import csv
+import math
+import copy
 
+class ROCK:
+    def __init__(self, data_binary):
+        self.data_binary = copy.deepcopy(data_binary)
+        self.similarity_matrix = None
+        self.adjacency_matrix = None
+        self.links_matrix = None
+        self.clusters = None
+        self.clusters_points = None
+        self.clusters_classes = None
+        self.function = None
 
-def jaccard_coefficient(a, b):
-    A = set(a)
-    B = set(b)
-    intersection = A.intersection(B)
-    sum = A.union(B)
-
-    if len(A) == 0 or len(B) == 0:
-        return 0
-    else:
-        return len(intersection)/len(sum)
-
-
-def get_similiarity_matrix(data):
-    points_num = len(data)
-    matrix = np.zeros((points_num,points_num))
-    for i in range(0, points_num):
-        a = data[i]
-        for j in range(i+1,points_num):
-            b = data[j]
-            matrix[i][j] = jaccard_coefficient(a,b)
-            matrix[j][i] = jaccard_coefficient(a,b)
-        matrix[i][i] = 1
-    return matrix
-
-
-def get_adjacency_matrix(data, phi):
-    points_num = len(data)
-    matrix = np.zeros((points_num,points_num))
-    similiarity_matrix = get_similiarity_matrix(data)
-    for i in range(0, points_num):
-        for j in range(i+1,points_num):
-            if similiarity_matrix[i][j] >= phi:
-                matrix[i][j] = 1
-                matrix[j][i] = 1
-        matrix[i][i] = 1
-    return matrix
-
-
-def get_links_matrix(adjacency_matrix):
-    links_matrix = np.matmul(adjacency_matrix, adjacency_matrix)
-    np.fill_diagonal(links_matrix,0)
-    return links_matrix
-
-
-def get_link_for_clusters(cluster_a, cluster_b, link_matrix):
-    links = 0
-    for point_a_index in cluster_a:
-        for point_b_index in cluster_b:
-            links += link_matrix[point_a_index][point_b_index]
-    return links
-
-
-def clusters_goodness(cluster_a, cluster_b, link_matrix, theta):
-    link_num = get_link_for_clusters(cluster_a, cluster_b, link_matrix)
-    n_a = len(cluster_a)
-    n_b = len(cluster_b)
-    f = (1-theta)/(1+theta)
-    a = pow((n_a + n_b), (1+2*f))
-    b = pow(n_a, 1+2*f)
-    c = pow(n_b, 1+2*f)
-    goodness = link_num / (pow((n_a + n_b), (1+2*f)) - pow(n_a, 1+2*f) - pow(n_b, 1+2*f)) 
-    return goodness
-
-
-def get_clusters_to_merge(all_clusters, link_matrix, theta):
-    max_goodness = 0
-    a_cluster_to_merge = None
-    b_cluster_to_merge = None 
-    for i in range(len(all_clusters)):
-        a = all_clusters[i]
-        for j in range(i+1, len(all_clusters)):
-            b = all_clusters[j]
-            goodness = clusters_goodness(a, b, link_matrix, theta)
-            if(goodness > max_goodness):
-                max_goodness = goodness
-                a_cluster_to_merge = i
-                b_cluster_to_merge = j
-    return (a_cluster_to_merge, b_cluster_to_merge)
-
-
-def rock(data, phi, cluster_num, theta = 0.5):
-    theta = phi
-    adjacency_matrix = get_adjacency_matrix(data, phi)
-    links_matrix = get_links_matrix(adjacency_matrix)
-    clusters = []
-    for i in range(len(data)):
-        clusters.append([i])
-
-    while (len(clusters) > cluster_num):
-        clusters_to_merge = get_clusters_to_merge(clusters, links_matrix, theta)
-        if clusters_to_merge != (None, None):
-            a = clusters_to_merge[0]
-            b = clusters_to_merge[1]
-            clusters[a] += clusters[b]
-            clusters.pop(b)
+    def get_intersection_sum_len(self, a, b):
+        intersection_len = 0
+        sum_len = 0
+        for i in range(len(a)):
+            if a[i] == b[i] and a[i] != 0 and b[i] != 0:
+                intersection_len += 1
+            if a[i] == 1 or b[i] == 1:
+                sum_len += 1
+        return (intersection_len, sum_len)
+    
+    def calculate_jaccard_coefficient(self, a, b):
+        if len(b) == 0 or len(a) == 0:
+            return 0
         else:
-            break
-    return clusters
+            intersection, sum = self.get_intersection_sum_len(a,b)
+            return intersection/sum
+        
+    def calculate_soresen_coefficient(self, a, b):
+        if len(b) == 0 or len(a) == 0:
+            return 0
+        else:
+            intersection, sum = self.get_intersection_sum_len(a,b)
+            sum_a = 0
+            sum_b = 0
+            
+            for i in range(len(a)):
+                sum_a += a[i]
+                sum_b += b[i] 
+            return 2*(intersection)/(sum_a+sum_b)
+        
+    def calculate_euclidean_distance(self, a, b):
+        sum = 0
+        for i in range(len(a)):
+            sum = sum + (int(a[i]) - int(b[i]))**2
+        euclidean_distance = math.sqrt(sum)
+        return euclidean_distance
+
+    def calculate_similiarity_matrix(self):
+        points_num = len(self.data_binary)
+        matrix = np.zeros((points_num,points_num))
+        for i in range(0, points_num):
+            for j in range(i+1,points_num):
+                if self.function == "jaccard":
+                    matrix[i][j] = self.calculate_jaccard_coefficient(self.data_binary[i][0], self.data_binary[j][0])
+                    matrix[j][i] = matrix[i][j]
+                elif self.function == "sorensen":
+                    matrix[i][j] = self.calculate_soresen_coefficient(self.data_binary[i][0], self.data_binary[j][0])
+                    matrix[j][i] = matrix[i][j]
+                elif self.function == "euclidean":
+                    matrix[i][j] = self.calculate_euclidean_distance(self.data_binary[i][0], self.data_binary[j][0])
+                    matrix[j][i] = matrix[i][j]
+            matrix[i][i] = 1
+
+        self.similarity_matrix = matrix
+
+    def calculate_adjacency_matrix(self, phi):
+        points_num = len(self.data_binary)
+        matrix = np.zeros((points_num,points_num))
+        self.calculate_similiarity_matrix()
+        for i in range(0, points_num):
+            for j in range(i+1,points_num):
+                if self.similarity_matrix[i][j] >= phi:
+                    matrix[i][j] = 1
+                    matrix[j][i] = 1
+            matrix[i][i] = 1
+        self.adjacency_matrix = matrix
+
+    def get_links_matrix(self, adjacency_matrix):
+        links_matrix = np.matmul(adjacency_matrix, adjacency_matrix)
+        np.fill_diagonal(links_matrix,0)
+        self.links_matrix = links_matrix
+   
+    def get_link_for_clusters(self, cluster_a, cluster_b):
+        links = 0
+        for point_a_index in cluster_a:
+            for point_b_index in cluster_b:
+                links += self.links_matrix[point_a_index][point_b_index]
+        return links
+
+    def clusters_goodness(self, cluster_a, cluster_b, theta):
+        link_num = self.get_link_for_clusters(cluster_a, cluster_b)
+        n_a = len(cluster_a)
+        n_b = len(cluster_b)
+        f = (1-theta)/(1+theta)
+        goodness = link_num / (pow((n_a + n_b), (1+2*f)) - pow(n_a, 1+2*f) - pow(n_b, 1+2*f)) 
+        return abs(goodness)
+
+    def get_clusters_to_merge(self, theta):
+        max_goodness = 0
+        a_cluster_to_merge = None
+        b_cluster_to_merge = None 
+        for i in range(len(self.clusters)):
+            a = self.clusters[i]
+            for j in range(i+1, len(self.clusters)):
+                b = self.clusters[j]
+                goodness = self.clusters_goodness(a, b, theta)
+                if(goodness >= max_goodness):
+                    max_goodness = goodness
+                    a_cluster_to_merge = i
+                    b_cluster_to_merge = j
+        return (a_cluster_to_merge, b_cluster_to_merge)
+
+    def get_clusters(self, theta, cluster_num, function = "jaccard", data = None):
+        if data: self.data_binary = data;
+        self.function = function
+
+        self.calculate_adjacency_matrix(theta)
+        self.get_links_matrix(self.adjacency_matrix)
+        self.clusters = []
+        for i in range(len(self.data_binary)):
+            self.clusters.append([i])
+
+        while (len(self.clusters) > cluster_num):
+            clusters_to_merge = self.get_clusters_to_merge(theta)
+            if clusters_to_merge != (None, None):
+                a = clusters_to_merge[0]
+                b = clusters_to_merge[1]
+                self.clusters[a] += self.clusters[b]
+                self.clusters.pop(b)
+            else:
+                break
+
+        for cluster in self.clusters:
+            cluster.sort()
+
+        self.get_clusters_points()
+        self.get_clusters_as_classes()
+        return self.clusters
+    
+    def get_clusters_points(self):
+        clusters_points = []
+        for cluster in self.clusters:
+            cluster_list = []
+            for point_index in cluster:
+                cluster_list.append(tuple(self.data_binary[point_index]))
+            clusters_points.append(cluster_list)
+        self.clusters_points = clusters_points
+
+    def get_clusters_as_classes(self):
+        clusters = []
+        for cluster in self.clusters_points:
+            cluster_classes = []
+            for point in cluster:
+                cluster_classes.append(point[1])
+            cluster_classes.sort()
+            clusters.append(cluster_classes)
+        self.clusters_classes = clusters
+        return clusters
+         
+    def get_score(self):
+        correctly_classified = 0
+        for cluster in self.clusters_classes:
+            most_frequent = max(set(cluster), key=cluster.count)
+            num_of_most_frequent = cluster.count(most_frequent)
+            correctly_classified += num_of_most_frequent
+        
+        return correctly_classified / len(self.data_binary)
 
 
-def run():
-    with open('dataset.csv') as file:
-        reader = csv.reader(file)
-        data = list(reader)
-        clusters = rock(data, 0.5, 2)
-        print(clusters)
-run()
+
--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 from scipy.io import arff
 import pandas as pd
-from ROCK_algorithm import ROCK
+from ROCK import ROCK
 from data_converter import Data_Converter
 from pyclustering.cluster.rock import rock
 
 data_converter = Data_Converter()
-
-# with open('dataset3.csv', newline='') as f:
-#     reader = csv.reader(f)
-#     example_data = list(reader)
-# for point in example_data:
-#     for i in range(len(point)):
-#         point[i] = int(point[i])
-# example_data = data_converter.separate_class_from_data(example_data, -1)
-
 
 zoo_data, meta = arff.loadarff('zoo.arff')
 zoo_data = pd.DataFrame(zoo_data)
@@ -22,49 +13,43 @@ zoo_data = data_converter.convert_data_types(zoo_data)
 zoo_data = zoo_data.values.tolist()
 zoo_data = data_converter.separate_class_from_data(zoo_data, -1)
 
-
 votes_data =  pd.read_csv('votes.data', sep=",", header = None)
-# print(votes_data.head())
-
 for i in range(1,17):
     votes_data = data_converter.split_multi_arg_attribute(votes_data,i,['y','n'], True)
-    # print(votes_data.head())
-# print(votes_data.head())
-
 votes_data = votes_data.values.tolist()
 votes_data = data_converter.separate_class_from_data(votes_data, 0)
 
 
-my_rock = ROCK(votes_data)
-
+my_rock = ROCK(zoo_data)
 jaccard = "jaccard"
 sorensen = "sorensen"
 euclidean = "euclidean"
 
 
-# clusters = my_rock.get_clusters(0.75 ,7, jaccard)
-# clusters_as_classes = my_rock.get_clusters_as_classes()
+# POJEDYNCZE URUCHOMIENIE
+clusters = my_rock.get_clusters(0.75 ,7, jaccard)
+clusters_as_classes = my_rock.get_clusters_as_classes()
+for cluster in clusters_as_classes:
+    print(cluster)
+    # print("No. republican", cluster.count('republican'))
+    # print("No. democrat", cluster.count('democrat'))
+print(my_rock.get_score())
 
-# for cluster in clusters_as_classes:
-#     print(cluster)
-#     print("No. republican", cluster.count('republican'))
-#     print("No. democrat", cluster.count('democrat'))
-i = 0.05
-while i < 1:
-    clusters = my_rock.get_clusters(i ,2, euclidean)
-    score = my_rock.get_score()
-    print("theta=" , round(i,2) , "score=" , round(score*100,1), "num_of_clusters" , len(my_rock.clusters))
+
+# # WIELOKROTNE URUCHOMIENIE
+# i = 0.05
+# while i < 1:
+#     clusters = my_rock.get_clusters(i ,7, jaccard)
+#     score = my_rock.get_score()
+#     print("theta=" , round(i,2) , "score=" , round(score*100,1), "num_of_clusters" , len(my_rock.clusters))
     
-    clusters_as_classes = my_rock.get_clusters_as_classes()
-    # for cluster in clusters_as_classes:
-    #     print(cluster)
+#     clusters_as_classes = my_rock.get_clusters_as_classes()
+#     for cluster in clusters_as_classes:
+#         print(cluster)
 
-    for cluster in clusters_as_classes:
-        # print(cluster)
-        print("No. republican", cluster.count('republican'))
-        print("No. democrat", cluster.count('democrat'))
+#     # for cluster in clusters_as_classes:
+#     #     # print(cluster)
+#     #     print("No. republican", cluster.count('republican'))
+#     #     print("No. democrat", cluster.count('democrat'))
 
-    i = i + 0.05
-# score = my_rock.get_score()
-# print(score)
-
+#     i = i + 0.05
